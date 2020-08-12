c206f7ecc388a545421c619191fecd660fdced6a
diyanaaivanova <dianaaivanova@gmail.com>
2020-08-12 14:13:28 +0300

    initial commit

---
+++ src/main/java/com/springguides/consumingaRESTfulwebservice/ConsumingAResTfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springguides.consumingaRESTfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingAResTfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingAResTfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springguides/consumingaRESTfulwebservice/ConsumingAResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springguides.consumingaRESTfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingAResTfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
